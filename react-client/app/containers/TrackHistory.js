6ea4d594989ddd5757a1992fef28bd46ba050a2e
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2016-04-17 20:21:55 -0400

    Added TrackEntry to TrackHistory container

--- react-client/app/containers/TrackHistory.js
+++ react-client/app/containers/TrackHistory.js
@@ -1,16 +1,19 @@
 var React = require('react');
+var TrackEntry = require('../components/TrackEntry');
+var style = require('../styles');
 
 var TrackHistory = React.createClass({
   render: function () {
     console.log("Props for TrackHistory ", this.props);
-    var listItems = this.props.tracks.map(function(friend){
-      return <li> {friend.pageURL} </li>;
+    var listItems = this.props.tracks.map(function(track){
+      // return <li> {track.pageURL} </li>;
+      return <TrackEntry url={ track.pageURL } name={ track.trackName } />
     });
     console.log(listItems);
     if (!listItems.length) listItems.push(<li>No tracks yet...</li>);
 
     return (
-      <ul>
+      <ul style={ style.trackList }>
           {listItems}
       </ul>
     )
